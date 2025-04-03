404eeca5559542d522d5ebc473522a622583f080
shelckk <pyshkin-danil@bk.ru>
2025-04-03 16:58:14 +0800

    inicial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,21 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+var dog = 8.0;
+var cat = 3.6;
+var paper = 763789;
+        System.out.println("dog =" + dog + ",cat =" + cat + ",paper =" + paper);
+dog = dog + 4;
+cat = cat + 4;
+paper = paper + 4;
+        System.out.println("dog =" + dog + ",cat =" + cat + ",paper =" + paper);
+var friend = 19;
+friend = (friend + 2) / 7;
+        System.out.println(friend);
+var firstBoxer = 78.2;
+var secondBoxer = 82.7;
+var delenie = secondBoxer % firstBoxer;
+        System.out.println(delenie);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
